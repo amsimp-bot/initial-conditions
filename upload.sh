01d7183ebc361f78c103ca187ff5e612bc36c612
Conor Casey <conorcaseyc@icloud.com>
2020-04-10 23:28:18 +0100

    Add files via upload

--- upload.sh
+++ upload.sh
@@ -2,13 +2,13 @@ for i in {1..30}
 do if [ $i -lt 10 ]
 then
     git add "0$i"
-    git commit "Maintain Historical Dataset"
+    git commit -m "Maintain Historical Dataset"
     git push
 fi 
 if [ $i -ge 10 ]
 then
     git add "$i"
-    git commit "Maintain Historical Dataset"
+    git commit -m "Maintain Historical Dataset"
     git push
 fi
 done

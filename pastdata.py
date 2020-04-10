8de7c61cd5d159f6f32cf516538fe63b20eff58b
Conor Casey <conorcaseyc@icloud.com>
2020-04-10 21:29:54 +0100

    Update pastdata.py

--- pastdata.py
+++ pastdata.py
@@ -12,7 +12,7 @@ from progress.bar import IncrementalBar
 # Retrieve current date.
 date = np.load('date.npy')
 date = datetime(int(date[2]), int(date[1]), int(date[0]), int(date[3]))
-date = date + timedelta(days=-100)
+date = date + timedelta(days=-60)
 
 # Create a bar to determine progress.
 max_bar = 60 * 4
@@ -62,14 +62,40 @@ for i in range(max_bar):
     data = iris.load(data_file)
 
     # Retrieve temperature, relative humdity, and geopotential height data.
-    temperature = data[58].data
-    rh = data[90].data
-    geopotential_height = data[77].data
+    temperature = data[58]
+    rh = data[90]
+    geopotential_height = data[77]
+    
+    # Save files.
+    folder = 'initial_conditions/' + year + "/" + month + '/' + day + '/' + hour + '/'
 
-    # Append to lists.
-    T_list.append(temperature)
-    rh_list.append(rh)
-    height_list.append(geopotential_height)
+    try:
+        os.mkdir('initial_conditions/'+year)
+    except OSError:
+        pass
+
+    try:
+        os.mkdir('initial_conditions/'+year+'/'+month)
+    except OSError:
+        pass
+
+    try:
+        os.mkdir('initial_conditions/' + year + "/" + month + '/' + day)
+    except OSError:
+        pass
+
+    try:
+        os.mkdir('initial_conditions/' + year + "/" + month + '/' + day + '/' + hour)
+    except OSError:
+        pass
+
+    # Save initial conditions data in .nc file
+    DataList = CubeList([
+        temperature,
+        rh,
+        geopotential_height,
+    ])
+    iris.save(DataList, folder + 'initial_conditions.nc')
     
     # Remove file to prevent clutter.
     os.remove(data_file)
@@ -77,21 +103,6 @@ for i in range(max_bar):
     bar.next()
   except HTTPError:
     pass
- 
-# Save initial conditions data in .npy file
-T = np.asarray(T_list)
-height = np.asarray(height_list)
-rh = np.asarray(rh_list)
-
-# Make folder.
-try:
-  os.mkdir('historical_conditions/')
-except OSError:
-  pass
-
-# Save.
-folder = 'historical_conditions/'
-np.savez_compressed(folder + 'rnn-data', a=T, b=height, c=rh)
 
 # Done.
 bar.finish()  

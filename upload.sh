e746a96f673a723efd2d9e6b74bfca95b195b17b
Conor Casey <conorcaseyc@icloud.com>
2020-04-10 22:50:37 +0100

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,14 @@
+for i in {1..30}
+do if [ $i -lt 10 ]
+then
+    git add "0$i"
+    git commit "Maintain Historical Dataset"
+    git push
+fi 
+if [ $i -ge 10 ]
+then
+    git add "$i"
+    git commit "Maintain Historical Dataset"
+    git push
+fi
+done
